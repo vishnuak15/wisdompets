c5e3e90ae6c979877cd076f03630ab7209bc25ad
Vishnu <vishnuak15@gmail.com>
2021-09-25 23:19:30 +0530

    templates

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,16 @@
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Pet
+from django.http import HttpResponse
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request,'home.html',{'pets': pets,})
+
+def pet_detial(request, pet_id):
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(request,'pet_detial.html',{'pets': pet,})
